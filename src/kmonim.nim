a4543803044ecfdaf5d4bb2f182af410cc3f3a51
Ross Hadden <rosshadden@gmail.com>
2022-08-10 03:00:35 -0400

    Added sexp `+`.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/kmonim.nim
+++ src/kmonim.nim
@@ -2,7 +2,26 @@ import nimsuggest/sexp
 
 import kmonim/kmonad
 
-when isMainModule:
-  let s = parseSexp ("(foo bar (baz (qux)))")
+proc `+`(lhs, rhs: SexpNode): SexpNode =
+  case lhs.kind:
+    of SList:
+      result = lhs.copy()
+      result.add(rhs)
+    else:
+      result = newSList()
+      result.add(lhs)
+      result.add(rhs)
 
-  echo repr s
+# proc thread(forms: varargs[string]
+
+when isMainModule:
+  let a = parseSexp ("(foo bar (baz (qux)))")
+  let b = parseSexp ("(a b \"c\")")
+  let c = a + b
+
+  echo "a:\t", a
+  echo "b:\t", b
+  echo "c:\t", c
+
+  echo c.pretty()
+  echo repr b
